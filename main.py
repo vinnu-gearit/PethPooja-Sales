1c566e7b0217b89fff7a632137a5ebcfbc2a4f50
vinay <vinay.gaonkar@diagonal.ai>
2025-02-19 12:56:13 +0530

    Trigger redeployment-11

--- main.py
+++ main.py
@@ -146,4 +146,4 @@ async def root():
 # import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level="debug")
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level="debug", timeout_keep_alive=120)
